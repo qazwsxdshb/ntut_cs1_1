9cbbfe0e270cc46b577f9dd89b12dd4258e4e48a
easn <a35744830@gmail.com>
2023-09-17 13:48:56 +0800

    2023/09/17

---
+++ hw008.py
@@ -0,0 +1,30 @@
+a=int(input())
+b=int(input())
+c=int(input())
+
+def str_to_arr(ans1):
+    tmp1=[]
+    tmp=""
+    for i in ans1:
+        if(i!="+" and i!="-" and i!="i"):
+            tmp+=i
+        else:
+            tmp1.append(tmp)
+            tmp1.append(i)
+            tmp=""
+    tmp1.append(tmp)
+    return tmp1
+
+ans1=str_to_arr(str(((-b+(b**2-(4*a*c))**0.5)/(2*a))).replace("(","").replace(")","").replace("j","i"))
+ans2=str_to_arr(str(((-b-(b**2-(4*a*c))**0.5)/(2*a))).replace("(","").replace(")","").replace("j","i"))
+for i in ans1:
+    try:
+        print("%.1f" %float(i),end="")
+    except:
+        print(i,end="")
+print()
+for i in ans2:
+    try:
+        print("%.1f" %float(i),end="")
+    except:
+        print(i,end="")
