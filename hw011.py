02f9cbcf3d3c989fbf4f473e32e3499a401027a7
easn <a35744830@gmail.com>
2023-09-18 22:12:46 +0800

    2023/09/18

--- hw011.py
+++ hw011.py
@@ -6,4 +6,7 @@ for _ in range(3):
     cla.append(x)
 
 for i in range(2):
-    [i]
+    for u in cla[i]:
+        for o in range(i+1,3):
+            if(u in cla[o]):
+                print(cla[i][0],",",cla[o][0],",",u,sep="")
