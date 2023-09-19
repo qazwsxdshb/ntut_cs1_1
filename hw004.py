9177c6b78b1566d9c61e86b410a49d739d8e14b4
easn <a35744830@gmail.com>
2023-09-19 15:25:08 +0800

    2023/09/19

--- hw004.py
+++ hw004.py
@@ -1,1 +1,8 @@
-print("even" if int(input())%2==0 else "odd")
+import math
+hi=float(input())
+kg=float(input())
+bmi=int(float(kg/(hi*hi))*1000)
+if(bmi%10==5 and ((bmi//10)%10)%2==1):
+    print("%.2f" %((bmi+10)//10/100)) 
+else:
+    print("%.2f" %((bmi//10)/100))
