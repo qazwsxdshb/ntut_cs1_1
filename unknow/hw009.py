e28bb013f4ff59f1360a2d1d83f7ec373a9e7087
easn <a35744830@gmail.com>
2023-09-26 13:44:17 +0800

    2023/09/26

--- unknow/hw009.py
+++ unknow/hw009.py
@@ -1,6 +1,10 @@
 pri=[[380,0.9,0.85,0.8],[1200,0.95,0.85,0.8],[180,0.85,0.8,0.7]]
 co,ans=0,0
-for i in map(int,input().split()):
+x=[]
+for i in range(3):
+    x.append(input())
+for i in x:
+    i=int(i)
     if(i<=20 and i>=11):
         ans+=i*pri[co][1]*pri[co][0]
     elif(i>=21 and i<=30):
