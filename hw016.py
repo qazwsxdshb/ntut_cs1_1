700b04c8c27fd4f410d401aec3aaf2e9b815ac76
easn <a35744830@gmail.com>
2023-10-04 22:15:12 +0800

    2023/10/04 use 20min

--- hw016.py
+++ hw016.py
@@ -1,8 +1,9 @@
 
 def getTriangle(ans: list):
-    tmp=(sum(ans)*(sum(ans)-ans[0])*(sum(ans)-ans[1])*(sum(ans)-ans[2]))**0.5
+    s=sum(ans)/2
+    tmp=(s*(s-ans[0])*(s-ans[1])*(s-ans[2]))**0.5
     ans=sorted(ans)[-1::-1]
-    if(ans[0]>=ans[1]+ans[2] or ans[2]<=0 or ans[1]<=0 or ans[0]<=0):return "not a triangle"
+    if(ans[0]>=ans[1]+ans[2] or ans[2]<=0 or ans[1]<=0 or ans[0]<=0):return "not a triangle","no"
     elif(ans[0]==ans[1] and ans[0]==ans[2] and ans[1]==ans[2]):return "equilateral triangle",tmp
     elif(ans[1]==ans[2] or ans[1]==ans[0] or ans[2]==ans[0]):return "isosceles triangle",tmp
     elif((ans[0]**2)==(ans[1]**2+ans[2]**2)):return "right triangle",tmp
@@ -12,9 +13,16 @@ def getTriangle(ans: list):
 ans=[]
 for _ in range(int(input())):
     ans.append([int(i) for i in input().split()])
+t=[]
 for i in range(len(ans)):
-    tmp=getTriangle(ans[i])
-    if(tmp[1] is not None):
-        print(tmp[0],tmp[1])
+    tmp,a=getTriangle(ans[i])
+    if(a != "no"):
+        t.append("%.2f" %a)
+        print(tmp,"%.2f" %a)
     else:
-        print(tmp[0])
+        print(tmp)
+if(len(t)==0):
+    print("All inputs are not triangles!")
+else:
+    print(max(t))
+    print(min(t))
