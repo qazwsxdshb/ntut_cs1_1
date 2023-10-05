55cc546f80dc6838812c0ec4fa0eaf581d263d36
easn <a35744830@gmail.com>
2023-10-05 14:27:39 +0800

    2023/10/05 use 2hr 感謝愷哥鼎力相助不愧是輕鬆滿分的人

--- hw014.py
+++ hw014.py
@@ -1,29 +1,32 @@
-def bowling():
-    x,p=[0 for _ in range(20)],0
-    for _ in range(10):
+x,p=[0]*20,0
+for _ in range(10):
+    x[p]=(int(input()))
+    p+=1
+    if(p%2==1 and x[p-1]==10):pass
+    elif(p%2==1):
         x[p]=(int(input()))
-        if(p%2==0 and x[p]==10):
-            p+=1
-        elif(p%2==0 and x[p]<10):
-            p+=1
-            x[p]=(int(input()))
-        p+=1
-    
-    if(x[-2]==10):
-        x.append(int(input()))
-        x.append(int(input()))
-    if(x[-1]+x[-2]==10):
-        x.append(int(input()))
-    
-    ans=sum(x)
+    p+=1
 
-    for i in range(0,18,2):
-        if(x[i]==10):
-            ans+=x[i+2]+x[i+3]
-        elif(x[i]+x[i+1]==10):
-            ans+=x[i+2]
-    
-    return ans
+if(x[-2]==10):
+    x.append(int(input()))
+    x.append(int(input()))
+elif(x[-1]+x[-2]==10):
+    x.append(int(input()))
 
-if __name__=='__main__':
-    print(bowling())
+ans=sum(x)
+
+for i in range(0,9):
+    if(x[i*2]==10):
+        l=0
+        for o in range(i*2+2,len(x)):
+            if(x[o]!=0):
+                l+=1
+                ans+=x[o]
+            if(l==2):
+                break
+    elif(x[i*2]+x[i*2+1]==10):
+        for o in range(i*2+2,len(x)):
+            if(x[o]!=0):
+                ans+=x[o]
+                break
+print(ans)
