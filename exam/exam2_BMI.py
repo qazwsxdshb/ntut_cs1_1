64c21eb8471d1e3688e29983b990213d4af39d79
easn <a35744830@gmail.com>
2023-11-07 13:16:54 +0800

    2023/11/07 edit

--- exam/exam2_BMI.py
+++ exam/exam2_BMI.py
@@ -1,16 +1,18 @@
+ans=[]
 try:
     while 1:
         height,weight=map(int,input().split())
         if(0.5>height*0.01 or height*0.01>2.5):
-            print("Input Height Error")
+            ans.append("Input Height Error")
         elif(20>weight*0.454 or weight*0.454>300):
-            print("Input Weight Error")
+            ans.append("Input Weight Error")
         else:
             BMI=(weight*0.454)/(height*0.01)**2
             if(BMI>24.00):
-                print("BMI too high")
+                ans.append("BMI too high")
             elif(18.50>BMI):
-                print("BMI too low")
+                ans.append("BMI too low")
             else:
-                print(int(BMI*100)/100)
-except:None
+                ans.append("%.2f" %(int(BMI*100)/100))
+except:
+    for i in ans:print(i)
