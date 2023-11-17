a49c86611e7e9fdbfed7417838d3102621f14574
easn <a35744830@gmail.com>
2023-11-17 10:18:39 +0800

    2023/11/17 use 2hr 題目理解好難

--- hw026.py
+++ hw026.py
@@ -5,17 +5,21 @@ class Solution:
     def ans(self):
         tmp_day=[0]*(self.recovery_day)
         tmp_day.append(self.first_positive)
-        total=self.first_positive
+        total,recover_total=self.first_positive,0
         for i in range(1,1+self.day):
             tmp=tmp_day.pop(0)
-            x=self.positive_person(total)*int(sum(tmp_day)) if i!=1 else 0
-            print(self.positive_person(total))
+            tt=int(sum(tmp_day)+tmp)
+            x=int(self.positive_person(recover_total,total,tt)) if i!=1 else 0
+            recover_total+=tmp
             total+=x
-            tmp_day.append(x)
+            tmp_day.append(x) if 1!=i else None
             print(i,int(sum(tmp_day)),int(self.first_positive) if i==1 else int(x),int(tmp))
-    
-    def positive_person(self,recovery_person):
-        return (self.spread/self.recovery_day)*(1-self.immunity-recovery_person/self.person)
+        print(total)
+
+    def positive_person(self,recovery_person,total,tt):
+        tmp=(self.spread/self.recovery_day)*(1-self.immunity-recovery_person/self.person) if int(self.person*self.immunity+total)<self.person else 0
+        ans=self.person-int(self.person*self.immunity+total) if int(self.person*self.immunity+total)+int(tmp*tt)>=self.person else tmp*tt
+        return ans
 
 if(__name__=="__main__"):
     ans=Solution(int(input()),int(input()),int(input()),float(input()),int(input()),float(input()))
