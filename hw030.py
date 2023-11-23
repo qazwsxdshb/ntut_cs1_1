3b5528135e3be4c9d65284249de1741e10f78977
easn <a35744830@gmail.com>
2023-11-23 20:34:16 +0800

    2023/11/23 use 10min

--- hw030.py
+++ hw030.py
@@ -7,11 +7,10 @@ def Solution(tmp,index=0):
         return Solution((tmp+1)//2,index+1)
 ans=[]
 while 1:
-    tmp=0
-    for i in range(int(input(),2)+1):
+    tmp,x=0,input()
+    if x=="-1":break
+    for i in range(int(x,2)+1):
         tmp+=Solution(i)
     ans.append(bin(tmp)[2:])
-    if input()=="-1":break
-print(ans)
 for i in ans:
     print("0"*(14-len(i))+i)
